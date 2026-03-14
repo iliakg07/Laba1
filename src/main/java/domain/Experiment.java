ca4f0b202655bf4ca9936a38de98a35f2a8fddfe
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-14 03:34:33 +0300

    make Experiment.java fields private

--- src/main/java/domain/Experiment.java
+++ src/main/java/domain/Experiment.java
@@ -4,17 +4,17 @@ import java.time.Instant;
 
 public final class Experiment {
     // Уникальный номер эксперимента. Программа назначает сама.
-    public long id;
+    private long id;
     // Название эксперимента. Нельзя пустое. До 128 символов.
-    public String name;
+    private String name;
     // Описание (кратко “что делаем”). Можно пусто. До 512 символов.
-    public String description;
+    private String description;
     // Кто создал (логин). На ранних этапах можно "SYSTEM".
-    public String ownerUsername;
+    private String ownerUsername;
     // Когда создан. Программа ставит автоматически.
-    public Instant createdAt;
+    private Instant createdAt;
     // Когда изменяли. Программа обновляет автоматически.
-    public Instant updatedAt;
+    private Instant updatedAt;
     }
 
 
