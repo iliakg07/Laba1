8de30c5a87832bedccfc57341b1e1df0024d15eb
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-14 12:15:34 +0300

    add constructors, getters & setters to Experiment

--- src/main/java/domain/Experiment.java
+++ src/main/java/domain/Experiment.java
@@ -6,15 +6,64 @@ public final class Experiment {
     // Уникальный номер эксперимента. Программа назначает сама.
     private long id;
     // Название эксперимента. Нельзя пустое. До 128 символов.
-    public String name;
+    private String name;
     // Описание (кратко “что делаем”). Можно пусто. До 512 символов.
-    public String description;
+    private String description;
     // Кто создал (логин). На ранних этапах можно "SYSTEM".
-    public String ownerUsername;
+    private String ownerUsername;
     // Когда создан. Программа ставит автоматически.
     private Instant createdAt;
     // Когда изменяли. Программа обновляет автоматически.
     private Instant updatedAt;
+
+    public Experiment(long id, String name, String description, String ownerUsername, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.ownerUsername = ownerUsername;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
 
 
--- src/main/java/validation/ExperimentValidator.java
+++ src/main/java/validation/ExperimentValidator.java
@@ -5,19 +5,19 @@ import domain.Experiment;
 public class ExperimentValidator {
     public static void validate(Experiment exp) {
 
-        if (exp.name == null) {
+        if (exp.getName() == null) {
             throw new ValidationException("Experiment name can't be empty");
         }
 
-        if (exp.name.length() >= 128) {
-            throw new ValidationException("Experiment name too long.");
+        if (exp.getName().length() >= 128) {
+            throw new ValidationException("Experiment name too long");
         }
 
-        if (exp.description != null && exp.description.length() > 512) {
+        if (exp.getDescription() != null && exp.getDescription().length() > 512) {
             throw new ValidationException("Description too long");
         }
 
-        if (exp.ownerUsername == null) {
+        if (exp.getOwnerUsername() == null) {
             throw new ValidationException("Owner username cannot be empty");
         }
     }
