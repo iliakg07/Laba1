0744003f43f90591f425e7e26f890558cbec0b4a
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-14 03:40:09 +0300

    add Run.java class

--- src/main/java/domain/Run.java
+++ src/main/java/domain/Run.java
@@ -1,4 +1,18 @@
 package domain;
 
-public class Run {
+import java.time.Instant;
+
+public final class Run {
+    // Уникальный номер запуска. Программа назначает сама.
+    private long id;
+    // К какому эксперименту относится (id эксперимента).
+    // Должен ссылаться на реально существующий Experiment.
+    private long experimentId;
+    // Название запуска reminder: “Run-2026-02-03-A”. Нельзя пустое. До 128 символов.
+    private String name;
+    // Кто выполнял запуск (логин или имя). Нельзя пустое. До 64 символов.
+    private String operatorName;
+    // Когда запуск зарегистрирован. Программа ставит автоматически.
+    private Instant createdAt;
 }
+
