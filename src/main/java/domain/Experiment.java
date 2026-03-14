5d4ac492d2d87c73bdc0b7e09a46e75b31d562d5
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-14 03:04:53 +0300

    add main method; create project structure - domain + validation + util + service

---
+++ src/main/java/domain/Experiment.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.time.Instant;
+
+public final class Experiment {
+    // Уникальный номер эксперимента. Программа назначает сама.
+    public long id;
+    // Название эксперимента. Нельзя пустое. До 128 символов.
+    public String name;
+    // Описание (кратко “что делаем”). Можно пусто. До 512 символов.
+    public String description;
+    // Кто создал (логин). На ранних этапах можно "SYSTEM".
+    public String ownerUsername;
+    // Когда создан. Программа ставит автоматически.
+    public Instant createdAt;
+    // Когда изменяли. Программа обновляет автоматически.
+    public Instant updatedAt;
+    }
+
+
---
+++ src/main/java/domain/Run.java
@@ -0,0 +1,4 @@
+package domain;
+
+public class Run {
+}
