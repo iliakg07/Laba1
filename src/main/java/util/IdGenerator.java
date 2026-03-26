286a4853237540fddd0700b33b119c9d518261a1
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-26 21:26:34 +0300

    add Id Generator & test to it

--- src/main/java/util/IdGenerator.java
+++ src/main/java/util/IdGenerator.java
@@ -1,4 +1,13 @@
 package util;
 
 public class IdGenerator {
+
+    private long nextId = 1;
+
+    public long next() {
+        return nextId++;
+    }
+
+//    Создаем переменную nextId и метод, который при каждом вызове возвращает +1 от старой переменной
+//    Т.е. 1 вызов = 1, 2 вызов = 2, 3 вызов = 3 и тд
 }
---
+++ src/test/java/util/IdGeneratorTest.java
@@ -0,0 +1,22 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdGeneratorTest {
+
+//    Тестим, генерирует ли метод последовательные айди
+
+    @Test
+    void shouldGenerateSequentialIds() {
+        IdGenerator generator = new IdGenerator();
+
+        long id1 = generator.next();
+        long id2 = generator.next();
+        long id3 = generator.next();
+
+        assertEquals(1, id1);
+        assertEquals(2, id2);
+        assertEquals(3, id3);
+    }
+}
