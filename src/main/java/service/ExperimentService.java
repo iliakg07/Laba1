7a57e515a1a8111a831c0dc8603929a5a811ab05
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-26 23:05:24 +0300

    add ExperimentService with add method

--- src/main/java/service/ExperimentService.java
+++ src/main/java/service/ExperimentService.java
@@ -1,4 +1,29 @@
 package service;
 
+import domain.Experiment;
+import util.IdGenerator;
+
+import java.util.TreeMap;
+
 public class ExperimentService {
+
+    private final TreeMap<Long, Experiment> experiments = new TreeMap<>(); // коллекция экспериментов
+    private final IdGenerator idGenerator = new IdGenerator();
+
+    public Experiment add(String name, String description, String ownerUsername) {
+
+        long id = idGenerator.next();
+
+        Experiment exp = new Experiment(
+                id,
+                name,
+                description,
+                ownerUsername,
+                java.time.Instant.now(),
+                java.time.Instant.now()
+        );
+
+        experiments.put(id, exp);
+        return exp;
+    }
 }
