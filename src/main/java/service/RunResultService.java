68a540c025153bbbbfdfba88f4c801c9e3ef4e73
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-27 21:13:38 +0300

    add RunResultService.add method

--- src/main/java/service/RunResultService.java
+++ src/main/java/service/RunResultService.java
@@ -1,12 +1,40 @@
 package service;
 
+import domain.MeasurementParam;
 import domain.RunResult;
 import util.IdGenerator;
+import validation.RunResultValidator;
 
+import java.time.Instant;
 import java.util.TreeMap;
 
 public class RunResultService {
 
     private final TreeMap<Long, RunResult> results = new TreeMap<>();
     private final IdGenerator idGenerator = new IdGenerator();
+
+    public RunResult add(long runId,
+                         MeasurementParam param,
+                         double value,
+                         String unit,
+                         String comment
+                         ) {
+
+        long id = idGenerator.next();
+
+        RunResult result = new RunResult(
+                id,
+                runId,
+                param,
+                value,
+                unit,
+                comment,
+                Instant.now()
+        );
+
+        RunResultValidator.validate(result);
+        results.put(id, result);
+        return result;
+
+    }
 }
