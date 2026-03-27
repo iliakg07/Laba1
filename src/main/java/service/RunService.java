fc84c489d72161c35221100ae506bc274e4fb292
mdtokarev277 <mdtokarev277@gmail.com>
2026-03-27 18:32:13 +0300

    create RunService.java & add RunService.add method

--- src/main/java/service/RunService.java
+++ src/main/java/service/RunService.java
@@ -1,4 +1,31 @@
 package service;
 
+import domain.Run;
+import util.IdGenerator;
+import validation.ValidationException;
+import validation.RunValidator;
+
+import java.time.Instant;
+import java.util.TreeMap;
+
 public class RunService {
+    private final TreeMap<Long, Run> runs = new TreeMap<>();
+    private final IdGenerator idGenerator = new IdGenerator();
+
+    public Run add(long experimentId, String name, String ownerUsername) {
+
+        long id = idGenerator.next();
+
+        Run run = new Run(
+                id,
+                experimentId,
+                name,
+                ownerUsername,
+                Instant.now()
+        );
+
+        RunValidator.validate(run);
+        runs.put(id, run);
+        return run;
+    }
 }
